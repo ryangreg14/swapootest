1f3a9752889485aa2eb2fa4e531783ab5982afc5
ryangreg14 <52232236+ryangreg14@users.noreply.github.com>
2019-12-04 15:20:49 +0800

    Create Install_jx.sh

---
+++ Install_jx.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -L "https://github.com/jenkins-x/jx/releases/download/$(curl --silent https://api.github.com/repos/jenkins-x/jx/releases/latest | jq -r '.tag_name')/jx-linux-amd64.tar.gz" | tar xzv "jx"
